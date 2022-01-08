09b944b454bc63aa97bf68290aba793825c3dd5a
kage <81172243+kage95@users.noreply.github.com>
2022-01-08 01:55:38 +0000

    [finish]4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,29 +1,23 @@
-class PostimagesController < ApplicationController
-  
+class PostImagesController < ApplicationController
   def new
-    @post_image = PostImage.new
+  end
+
+  def index
   end
   
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to new_post_image_path
-  end
-
-  def index
+    redirect_to post_images_path
   end
 
   def show
   end
   
-  def destroy
-  end
-  
-  private
-  
+    private
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
 end
